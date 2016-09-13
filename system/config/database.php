456457173d01eae5c14efdcbdaac28460bb72997
GplCart <gplcart.software@gmail.com>
2016-09-13 03:15:02 +0300

    Update database.php

--- system/config/database.php
+++ system/config/database.php
@@ -1,6 +1,13 @@
 <?php
 
 /**
+ * @package GPL Cart core
+ * @author Iurii Makukh <gplcart.software@gmail.com>
+ * @copyright Copyright (c) 2015, Iurii Makukh
+ * @license https://www.gnu.org/licenses/gpl.html GNU/GPLv3
+ */
+
+/**
  * Contains an array of database scheme used to create tables upon installation
  */
 $tables = array();
