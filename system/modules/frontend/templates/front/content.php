6234bc0a486b003bd724aca0a3c0241bacb092e3
GplCart <gplcart.software@gmail.com>
2017-01-01 19:13:57 +0700

    Removed all .php templates  from frontend theme

--- system/modules/frontend/templates/front/content.php
+++
@@ -1,9 +0,0 @@
-<?php if (!empty($collection_banners)) { ?>
-<?php echo $collection_banners; ?>
-<?php } ?>
-<?php if (!empty($collection_products)) { ?>
-<?php echo $collection_products; ?>
-<?php } ?>
-<?php if (!empty($collection_pages)) { ?>
-<?php echo $collection_pages; ?>
-<?php } ?>
