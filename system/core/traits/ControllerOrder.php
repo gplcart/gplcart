4f33bcc71276a361d49b09d4848d73cf0a632efd
GplCart <gplcart.software@gmail.com>
2017-05-18 02:59:13 +0300

    Refactoring traits

--- system/core/traits/ControllerOrder.php
+++ system/core/traits/ControllerOrder.php
@@ -16,204 +16,78 @@ trait ControllerOrder
 {
 
     /**
-     * Adds extra data to the order
-     * @param \gplcart\core\Controller $controller
+     * Prepare shipping method
      * @param array $order
-     * @return array
+     * @param \gplcart\core\models\Shipping $model
+     * @param \gplcart\core\Controller $controller
      */
-    protected function prepareOrderTrait($controller, array &$order)
+    protected function prepareOrderShippingTrait(&$order, $model, $controller)
     {
-        if (!$controller instanceof \gplcart\core\Controller) {
-            throw new \RuntimeException("Object is not instance of \gplcart\core\Controller");
-        }
+        $data = $model->get($order['shipping']);
+        $order['shipping_name'] = empty($data['title']) ? $controller->text('Unknown') : $data['title'];
+    }
 
-        /* @var $shipping_model \gplcart\core\models\Shipping */
-        $shipping_model = $controller->prop('shipping');
-        /* @var $payment_model \gplcart\core\models\Payment */
-        $payment_model = $controller->prop('payment');
-        /* @var $store_model \gplcart\core\models\Store */
-        $store_model = $controller->prop('store');
-        /* @var $order_model \gplcart\core\models\Order */
-        $order_model = $controller->prop('order');
-        /* @var $price_model \gplcart\core\models\Price */
-        $price_model = $controller->prop('price');
-        /* @var $address_model \gplcart\core\models\Address */
-        $address_model = $controller->prop('address');
+    /**
+     * Prepare order payment method
+     * @param array $order
+     * @param \gplcart\core\models\Payment $model
+     * @param \gplcart\core\Controller $controller
+     */
+    protected function prepareOrderPaymentTrait(&$order, $model, $controller)
+    {
+        $data = $model->get($order['payment']);
+        $order['payment_name'] = empty($data['title']) ? $controller->text('Unknown') : $data['title'];
+    }
 
-        $store = $store_model->get($order['store_id']);
-        $payment = $payment_model->get($order['payment']);
-        $shipping = $shipping_model->get($order['shipping']);
-        $status = $order_model->getStatusName($order['status']);
+    /**
+     * Prepare order store
+     * @param array $order
+     * @param \gplcart\core\models\Store $model
+     * @param \gplcart\core\Controller $controller
+     */
+    protected function prepareOrderStoreTrait(&$order, $model, $controller)
+    {
+        $data = $model->get($order['store_id']);
+        $order['store_name'] = empty($data['name']) ? $controller->text('Unknown') : $data['name'];
+    }
 
+    /**
+     * Prepare order status
+     * @param array $order
+     * @param \gplcart\core\models\Order $model
+     * @param \gplcart\core\Controller $controller
+     */
+    protected function prepareOrderStatusTrait(&$order, $model, $controller)
+    {
+        $data = $model->getStatusName($order['status']);
+        $order['status_name'] = empty($data) ? $controller->text('Unknown') : $data;
+    }
+
+    /**
+     * Prepare order total
+     * @param array $order
+     * @param \gplcart\core\models\Price $model
+     */
+    protected function prepareOrderTotalTrait(&$order, $model)
+    {
+        $order['total_formatted'] = $model->format($order['total'], $order['currency']);
+    }
+
+    /**
+     * Prepare order addresses
+     * @param array $order
+     * @param \gplcart\core\models\Address $model
+     */
+    protected function prepareOrderAddressTrait(&$order, $model)
+    {
         $order['address'] = array();
         foreach (array('shipping', 'payment') as $type) {
-            $address = $address_model->get($order["{$type}_address"]);
+            $address = $model->get($order["{$type}_address"]);
             if (!empty($address)) {
                 $order['address'][$type] = $address;
-                $order['address_translated'][$type] = $address_model->getTranslated($order['address'][$type], true);
+                $order['address_translated'][$type] = $model->getTranslated($order['address'][$type], true);
             }
         }
-
-        $order['status_name'] = empty($status) ? $controller->text('Unknown') : $status;
-        $order['store_name'] = empty($store['name']) ? $controller->text('Unknown') : $store['name'];
-        $order['payment_name'] = empty($payment['title']) ? $controller->text('Unknown') : $payment['title'];
-        $order['shipping_name'] = empty($shipping['title']) ? $controller->text('Unknown') : $shipping['title'];
-
-        $order['total_formatted'] = $price_model->format($order['total'], $order['currency']);
-        return $order;
-    }
-
-    /**
-     * Returns an array of prepared order components
-     * @param \gplcart\core\Controller $controller
-     * @param array $order
-     * @param string $dir
-     * @return array
-     */
-    protected function prepareOrderComponentsTrait($controller, &$order, $dir)
-    {
-        if (!$controller instanceof \gplcart\core\Controller) {
-            throw new \RuntimeException("Object is not instance of \gplcart\core\Controller");
-        }
-
-        if (empty($order['data']['components'])) {
-            return array();
-        }
-
-        foreach (array_keys($order['data']['components']) as $type) {
-            $this->prepareOrderComponentsCartTrait($controller, $order, $type, $dir);
-            $this->prepareOrderComponentsMethodTrait($controller, $order, $type, $dir);
-            $this->prepareOrderComponentsPriceRuleTrait($controller, $order, $type, $dir);
-        }
-
-        ksort($order['data']['components']);
-        return $order['data']['components'];
-    }
-
-    /**
-     * Sets rendered component "Cart"
-     * @param \gplcart\core\Controller $controller
-     * @param array $order
-     * @param string $type
-     * @param string $dir
-     * @return null
-     */
-    protected function prepareOrderComponentsCartTrait($controller, &$order,
-            $type, $dir)
-    {
-
-        if (!$controller instanceof \gplcart\core\Controller) {
-            throw new \RuntimeException("Object is not instance of \gplcart\core\Controller");
-        }
-
-        if ($type !== 'cart') {
-            return null;
-        }
-
-        /* @var $price_model \gplcart\core\models\Price */
-        $price_model = $controller->prop('price');
-
-        foreach ($order['data']['components']['cart'] as $sku => $price) {
-            if ($order['cart'][$sku]['product_store_id'] != $order['store_id']) {
-                $order['cart'][$sku]['product_status'] = 0;
-            }
-            $order['cart'][$sku]['price_formatted'] = $price_model->format($price, $order['currency']);
-        }
-
-        $data = array('order' => $order);
-        $html = $controller->render("$dir/cart", $data);
-        $order['data']['components']['cart']['rendered'] = $html;
-    }
-
-    /**
-     * Sets rendered shipping/payment component
-     * @param \gplcart\core\Controller $controller
-     * @param array $order
-     * @param string $type
-     * @param string $dir
-     * @return null
-     */
-    protected function prepareOrderComponentsMethodTrait($controller, &$order,
-            $type, $dir)
-    {
-
-        if (!$controller instanceof \gplcart\core\Controller) {
-            throw new \RuntimeException("Object is not instance of \gplcart\core\Controller");
-        }
-
-        /* @var $shipping_model \gplcart\core\models\Shipping */
-        $shipping_model = $controller->prop('shipping');
-        /* @var $payment_model \gplcart\core\models\Payment */
-        $payment_model = $controller->prop('payment');
-        /* @var $price_model \gplcart\core\models\Price */
-        $price_model = $controller->prop('price');
-
-        if ($type == 'shipping') {
-            $method = $shipping_model->get($order['shipping']);
-        } else if ($type == 'payment') {
-            $method = $payment_model->get($order['payment']);
-        } else {
-            return null;
-        }
-
-        if (empty($method['name'])) {
-            $method['name'] = $controller->text('Unknown');
-        }
-
-        $price = $order['data']['components'][$type];
-
-        if (abs($price) == 0) {
-            $price = 0;
-        }
-
-        $method['price_formatted'] = $price_model->format($price, $order['currency']);
-
-        $html = $controller->render("$dir/method", array('method' => $method));
-        $order['data']['components'][$type]['rendered'] = $html;
-    }
-
-    /**
-     * Sets rendered rule component
-     * @param \gplcart\core\Controller $controller
-     * @param array $order
-     * @param string $type
-     * @param string $dir
-     * @return null
-     */
-    protected function prepareOrderComponentsPriceRuleTrait($controller,
-            &$order, $type, $dir)
-    {
-
-        if (!$controller instanceof \gplcart\core\Controller) {
-            throw new \RuntimeException("Object is not instance of \gplcart\core\Controller");
-        }
-
-        if (!is_numeric($type)) {
-            return null; // Numeric type = price rule ID
-        }
-
-        /* @var $price_model \gplcart\core\models\Price */
-        $price_model = $controller->prop('price');
-        /* @var $pricerule_model \gplcart\core\models\PriceRule */
-        $pricerule_model = $controller->prop('pricerule');
-
-        $rule = $pricerule_model->get($type);
-        $price = $order['data']['components'][$type];
-
-        if (abs($price) == 0) {
-            $price = 0;
-        }
-
-        $data = array(
-            'rule' => $rule,
-            'price' => $price_model->format($price, $rule['currency'])
-        );
-
-        $html = $controller->render("$dir/rule", $data);
-
-        $order['data']['components'][$type] = array(
-            'rendered' => $html,
-            'price' => $order['data']['components'][$type]);
     }
 
 }
---
+++ system/core/traits/ControllerOrderComponent.php
@@ -0,0 +1,133 @@
+<?php
+
+/**
+ * @package GPL Cart core
+ * @author Iurii Makukh <gplcart.software@gmail.com>
+ * @copyright Copyright (c) 2015, Iurii Makukh
+ * @license https://www.gnu.org/licenses/gpl.html GNU/GPLv3
+ */
+
+namespace gplcart\core\traits;
+
+/**
+ * Contains controller methods for order components
+ */
+trait ControllerOrderComponent
+{
+
+    /**
+     * Prepare cart component
+     * @param array $order
+     * @param \gplcart\core\Controller $controller
+     * @param \gplcart\core\models\Price $price
+     */
+    protected function prepareOrderComponentCartTrait(&$order, $controller,
+            $price)
+    {
+        if (empty($order['data']['components']['cart'])) {
+            return null;
+        }
+
+        foreach ($order['data']['components']['cart'] as $sku => $price) {
+            if ($order['cart'][$sku]['product_store_id'] != $order['store_id']) {
+                $order['cart'][$sku]['product_status'] = 0;
+            }
+            $order['cart'][$sku]['price_formatted'] = $price->format($price, $order['currency']);
+        }
+
+        $html = $controller->render('sale/order/panes/components/cart', array('order' => $order));
+        $order['data']['components']['cart']['rendered'] = $html;
+    }
+
+    /**
+     * Prepare shipping method component
+     * @param array $order
+     * @param \gplcart\core\Controller $controller
+     * @param \gplcart\core\models\Price $price
+     * @param \gplcart\core\models\Shipping $shipping
+     */
+    protected function prepareOrderComponentShippingMethodTrait(&$order,
+            $controller, $price, $shipping)
+    {
+        if (!isset($order['data']['components']['shipping'])) {
+            return null;
+        }
+
+        $method = $shipping->get($order['shipping']);
+
+        if (empty($method['name'])) {
+            $method['name'] = $controller->text('Unknown');
+        }
+
+        $value = $order['data']['components']['shipping'];
+
+        if (abs($value) == 0) {
+            $value = 0;
+        }
+
+        $method['price_formatted'] = $price->format($value, $order['currency']);
+        $html = $controller->render('sale/order/panes/components/method', array('method' => $method));
+        $order['data']['components']['shipping']['rendered'] = $html;
+    }
+
+    /**
+     * Prepare payment method component
+     * @param array $order
+     * @param \gplcart\core\Controller $controller
+     * @param \gplcart\core\models\Price $price
+     * @param \gplcart\core\models\Payment $payment
+     */
+    protected function prepareOrderComponentPaymentMethodTrait(&$order,
+            $controller, $price, $payment)
+    {
+        if (!isset($order['data']['components']['payment'])) {
+            return null;
+        }
+
+        $method = $payment->get($order['payment']);
+
+        if (empty($method['name'])) {
+            $method['name'] = $controller->text('Unknown');
+        }
+
+        $value = $order['data']['components']['payment'];
+
+        if (abs($value) == 0) {
+            $value = 0;
+        }
+
+        $method['price_formatted'] = $price->format($value, $order['currency']);
+        $html = $controller->render('sale/order/panes/components/method', array('method' => $method));
+        $order['data']['components']['payment']['rendered'] = $html;
+    }
+
+    /**
+     * Prepare price rule component
+     * @param array $order
+     * @param \gplcart\core\Controller $controller
+     * @param \gplcart\core\models\Price $price
+     * @param \gplcart\core\models\PriceRule $pricerule
+     */
+    protected function prepareOrderComponentPriceRuleTrait(&$order, $controller,
+            $price, $pricerule)
+    {
+        foreach (array_keys($order['data']['components']) as $type) {
+
+            if (!is_numeric($type)) {
+                continue;
+            }
+
+            $rule = $pricerule->get($type);
+            $value = $order['data']['components'][$type];
+
+            if (abs($value) == 0) {
+                $value = 0;
+            }
+
+            $data = array('rule' => $rule, 'price' => $price->format($value, $rule['currency']));
+            $html = $controller->render('sale/order/panes/components/rule', $data);
+            $order['data']['components'][$type] = array('rendered' => $html, 'price' => $value);
+        }
+    }
+
+}
