223928fb0f2d112c5f56cc05889b3ded1fd94d93
GplCart <gplcart.software@gmail.com>
2017-07-31 00:23:32 +0300

    Add new handy methods to core\Module

--- system/core/Module.php
+++ system/core/Module.php
@@ -47,11 +47,57 @@ abstract class Module
 
     /**
      * Returns a class instance
+     * @param $string Either module ID or namespaced class name
      * @return object
      */
-    protected function getInstance($class)
+    protected function getInstance($string)
     {
-        return Container::get($class);
+        if (strpos($string, 'gplcart\\') === 0) {
+            return Container::get($string);
+        }
+
+        return $this->config->getModuleInstance($string);
+    }
+
+    /**
+     * Returns a class instance for the given module and model name
+     * @param string $model
+     * @param string|null $module_id
+     * @return object
+     */
+    protected function getModel($model, $module_id = null)
+    {
+        if (!isset($module_id)) {
+            return $this->getInstance("gplcart\\core\\models\\$model");
+        }
+
+        $basenamespace = $this->config->getModuleBaseNamespace($module_id);
+        return $this->getInstance("$basenamespace\\models\\$model");
+    }
+
+    /**
+     * Returns a class instance for the given module and handler name
+     * @param string $helper
+     * @param string|null $module_id
+     * @return object
+     */
+    protected function getHelper($helper, $module_id = null)
+    {
+        if (!isset($module_id)) {
+            return $this->getInstance("gplcart\\core\\helpers\\$helper");
+        }
+
+        $basenamespace = $this->config->getModuleBaseNamespace($module_id);
+        return $this->getInstance("$basenamespace\\helpers\\$helper");
+    }
+
+    /**
+     * Returns library instance
+     * @return \gplcart\core\Library
+     */
+    protected function getLibrary()
+    {
+        return $this->getInstance('gplcart\\core\\Library');
     }
 
 }
