d6a0550b7bad80f9baec30bc1aedba4d86b069f6
GplCart <gplcart.software@gmail.com>
2017-08-02 22:45:10 +0300

    Remove "tracking_status" field from database scheme

--- system/config/default/database.php
+++ system/config/default/database.php
@@ -261,7 +261,6 @@ return array(
             'payment' => array('type' => 'varchar', 'length' => 255, 'not_null' => true),
             'shipping' => array('type' => 'varchar', 'length' => 255, 'not_null' => true),
             'tracking_number' => array('type' => 'varchar', 'length' => 255, 'not_null' => true, 'default' => ''),
-            'tracking_status' => array('type' => 'varchar', 'length' => 255, 'not_null' => true, 'default' => ''),
             'status' => array('type' => 'varchar', 'length' => 50, 'not_null' => true),
             'comment' => array('type' => 'text', 'not_null' => true, 'default' => ''),
             'volume' => array('type' => 'float', 'default' => 0),
