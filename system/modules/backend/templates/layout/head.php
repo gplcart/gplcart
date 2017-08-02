da29628b45432c95b8a4a04f92655a6f5d68bf32
GplCart <gplcart.software@gmail.com>
2017-08-02 18:26:37 +0300

    Check vars exist in layout\head

--- system/modules/backend/templates/layout/head.php
+++ system/modules/backend/templates/layout/head.php
@@ -7,18 +7,26 @@
  */
 ?>
 <head>
+  <?php if(!empty($_meta_tags)) { ?>
   <?php foreach ($_meta_tags as $tag) { ?>
   <meta<?php echo $this->attributes($tag); ?>>
   <?php } ?>
+  <?php } ?>
+  <?php if(!empty($_head_title)) { ?>
   <title><?php echo $_head_title; ?></title>
+  <?php } ?>
+  <?php if(!empty($_styles)) { ?>
   <?php foreach ($_styles as $data) { ?>
   <link href="<?php echo $this->e($data['key']); ?>" rel="stylesheet">
   <?php } ?>
+  <?php } ?>
+  <?php if(!empty($_scripts_top)) { ?>
   <?php foreach ($_scripts_top as $data) { ?>
     <?php if (!empty($data['text'])) { ?>
     <script><?php echo $data['asset']; ?></script>
     <?php } else { ?>
     <script src="<?php echo $this->e($data['key']); ?>"></script>
     <?php } ?>
+  <?php } ?>
   <?php } ?>
 </head>
