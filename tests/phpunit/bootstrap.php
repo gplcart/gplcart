36d1bdf92c5f31518a8244c1259edd90de57f220
GplCart <gplcart.software@gmail.com>
2017-09-06 20:48:25 +0300

    Update testing stuff

--- tests/phpunit/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * @package GPL Cart core
- * @author Iurii Makukh <gplcart.software@gmail.com>
- * @copyright Copyright (c) 2015, Iurii Makukh
- * @license https://www.gnu.org/licenses/gpl.html GNU/GPLv3
- */
-require_once __DIR__ . '/../../system/bootstrap.php';
