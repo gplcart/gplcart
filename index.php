88a513b42de0c2d5ede2b0f5c9875a3df834b9d5
GplCart <gplcart.software@gmail.com>
2017-09-19 21:59:01 +0300

    Direct call Facade::routeHttp()

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php
 
-use gplcart\core\Container;
-
 /**
  * @package GPL Cart core
  * @author Iurii Makukh <gplcart.software@gmail.com>
  * @copyright Copyright (c) 2015, Iurii Makukh
  * @license https://www.gnu.org/licenses/gpl.html GNU/GPLv3
  */
+use gplcart\core\Container;
+
 require 'system/bootstrap.php';
 
 /* @var $facade \gplcart\core\Facade */
 $facade = Container::get('gplcart\\core\\Facade');
-$facade->route();
+$facade->routeHttp();
 
