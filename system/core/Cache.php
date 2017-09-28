7fd823bf5b7c27b4882aae41f441800d2cfbd9ac
GplCart <gplcart.software@gmail.com>
2017-09-29 00:41:03 +0300

    Add hooks to Cache class + refactoring

--- system/core/Cache.php
+++ system/core/Cache.php
@@ -16,16 +16,30 @@ class Cache
 {
 
     /**
-     * Cache file modification time
-     * @var integer
+     * Hook class instance
+     * @var \gplcart\core\Hook $hook
      */
-    protected $filemtime;
+    protected $hook;
 
     /**
      * Cache file path
      * @var string
      */
     protected $file;
+
+    /**
+     * Cache file modification time
+     * @var integer
+     */
+    protected $filemtime;
+
+    /**
+     * @param Hook $hook
+     */
+    public function __construct(Hook $hook)
+    {
+        $this->hook = $hook;
+    }
 
     /**
      * Return cache file modification time
@@ -53,15 +67,27 @@ class Cache
      */
     public function set($cid, $data)
     {
-        $key = gplcart_cache_key($cid);
-        $this->file = GC_CACHE_DIR . "/$key.cache";
+        $result = null;
+        $this->hook->attach('cache.set.before', $cid, $data, $result, $this);
 
-        if (file_put_contents($this->file, serialize((array) $data)) !== false) {
-            chmod($this->file, 0600);
-            return true;
+        if (isset($result)) {
+            return $result;
         }
 
-        return false;
+        if (is_array($cid)) {
+            $cid = gplcart_array_hash($cid);
+        }
+
+        $this->file = GC_CACHE_DIR . "/$cid.cache";
+
+        $result = false;
+        if (file_put_contents($this->file, serialize((array) $data)) !== false) {
+            chmod($this->file, 0600);
+            $result = true;
+        }
+
+        $this->hook->attach('cache.set.after', $cid, $data, $result, $this);
+        return (bool) $result;
     }
 
     /**
@@ -72,46 +98,66 @@ class Cache
      */
     public function get($cid, $options = array())
     {
+        $result = null;
         $options += array('default' => null, 'lifespan' => 0);
+        $this->hook->attach('cache.get.before', $cid, $options, $result, $this);
 
-        $key = gplcart_cache_key($cid);
-        $this->file = GC_CACHE_DIR . "/$key.cache";
-
-        if (!file_exists($this->file)) {
-            return $options['default'];
+        if (isset($result)) {
+            return $result;
         }
 
-        $this->filemtime = filemtime($this->file);
-
-        $fresh = true;
-        if (!empty($options['lifespan'])) {
-            $fresh = ($this->filemtime > (GC_TIME - $options['lifespan']));
+        if (is_array($cid)) {
+            $cid = gplcart_array_hash($cid);
         }
 
-        if (!$fresh) {
-            return $options['default'];
+        $this->file = GC_CACHE_DIR . "/$cid.cache";
+
+        $result = $options['default'];
+
+        if (is_file($this->file)) {
+            $this->filemtime = filemtime($this->file);
+            if (empty($options['lifespan']) || ((GC_TIME - $this->filemtime) < $options['lifespan'])) {
+                $result = unserialize(file_get_contents($this->file));
+            }
         }
 
-        return unserialize(file_get_contents($this->file));
+        $this->hook->attach('cache.get.after', $cid, $options, $result, $this);
+        return $result;
     }
 
     /**
      * Clears a cached data
      * @param string|null|array $cid
      * @param array $options
+     * @return bool
      */
     public function clear($cid, $options = array())
     {
+        $result = null;
         $options += array('pattern' => '.cache');
+        $this->hook->attach('cache.clear.before', $cid, $options, $result, $this);
 
-        gplcart_static_clear($cid);
+        if (isset($result)) {
+            return $result;
+        }
+
+        $result = true;
 
         if ($cid === null) {
             array_map('unlink', glob(GC_CACHE_DIR . '/*.cache'));
+            gplcart_static_clear();
+        } else {
+
+            if (is_array($cid)) {
+                $cid = gplcart_array_hash($cid);
+            }
+
+            array_map('unlink', glob(GC_CACHE_DIR . "/$cid{$options['pattern']}"));
+            gplcart_static_clear($cid);
         }
 
-        $key = gplcart_cache_key($cid);
-        array_map('unlink', glob(GC_CACHE_DIR . "/$key{$options['pattern']}"));
+        $this->hook->attach('cache.clear.after', $cid, $options, $result, $this);
+        return (bool) $result;
     }
 
 }
