81e21bce57ba0587e23e5642ce59f0b64250d135
GplCart <gplcart.software@gmail.com>
2017-11-09 20:47:12 +0200

    Update Help model

--- system/core/models/Help.php
+++ system/core/models/Help.php
@@ -41,19 +41,63 @@ class Help
         $content = file_get_contents($file);
 
         $matches = array();
-        preg_match_all('~<!--(.*?)-->~msi', $content, $matches);
+        preg_match('~<!--(.*?)-->~msi', $content, $matches);
 
-        $meta = array('title' => '', 'teaser' => '');
-
-        if (isset($matches[1][0])) {
-            $meta['title'] = trim($matches[1][0]);
+        if (empty($matches[1])) {
+            return array();
         }
 
-        if (isset($matches[1][1])) {
-            $meta['teaser'] = trim($matches[1][1]);
+        $meta = json_decode(trim($matches[1]), true);
+
+        if (!is_array($meta)) {
+            return array();
         }
+
+        $meta += array(
+            'title' => '',
+            'teaser' => '',
+            'weight' => 0
+        );
 
         return $meta;
+    }
+
+    /**
+     * Returns an array of help data from the file
+     * @param string $file
+     * @return array
+     */
+    public function get($file)
+    {
+        $data = $this->getMeta($file);
+
+        if (empty($data)) {
+            return array();
+        }
+
+        $hash = gplcart_string_encode(gplcart_path_relative($file));
+
+        $data += array(
+            'file' => $file,
+            'path' => "admin/help/$hash"
+        );
+
+        $this->hook->attach('help.get', $file, $data, $this);
+        return $data;
+    }
+
+    /**
+     * Returns a path to a directory containing help files for the given language
+     * @param string $langcode
+     * @return string
+     */
+    public function getDirectory($langcode)
+    {
+        if (empty($langcode)) {
+            $langcode = 'en';
+        }
+
+        return GC_DIR_HELP . "/$langcode";
     }
 
     /**
@@ -67,30 +111,53 @@ class Help
             $langcode = 'en';
         }
 
-        $dir = GC_DIR_HELP . "/$langcode";
+        $list = &gplcart_static("help.list.$langcode");
 
-        if (!is_dir($dir) || !is_readable($dir)) {
+        if (isset($list)) {
+            return $list;
+        }
+
+        $dir = $this->getDirectory($langcode);
+
+        if (!is_dir($dir)) {
             return array();
         }
 
         $files = glob("$dir/*.php");
 
-        $list = array();
+        $list = $titles = $weights = array();
+
         foreach ($files as $file) {
-            $meta = $this->getMeta($file);
-            if (!empty($meta['title'])) {
-                $list[] = array(
-                    'file' => $file,
-                    'title' => $meta['title'],
-                    'teaser' => $meta['teaser'],
-                    'hash' => gplcart_string_encode(gplcart_path_relative($file)));
+            $data = $this->get($file);
+            if (!empty($data['title'])) {
+                $list[] = $data;
+                $titles[] = $data['title'];
+                $weights[] = $data['weight'];
             }
         }
 
-        gplcart_array_sort($list, 'title');
-
-        $this->hook->attach('help.list', $list, $this);
+        array_multisort($weights, SORT_ASC, $titles, SORT_ASC, $list);
         return $list;
+    }
+
+    /**
+     * Returns a help data using a simple path pattern, e.g admin/*
+     * @param string $pattern
+     * @param string $langcode
+     * @return array
+     */
+    public function getByPattern($pattern, $langcode)
+    {
+        $prepared = strtr($pattern, array('/' => '-', '*' => '_', '/*' => '_'));
+
+        foreach (array($langcode, 'en') as $code) {
+            $dir = $this->getDirectory($code);
+            if (is_file("$dir/$prepared.php")) {
+                return $this->get("$dir/$prepared.php");
+            }
+        }
+
+        return array();
     }
 
 }
