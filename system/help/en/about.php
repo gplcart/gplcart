8a1cfbce2910d17e7171c42611fb457ffa2021ea
GplCart <gplcart.software@gmail.com>
2017-11-09 20:49:21 +0200

    Add "About" help file

---
+++ system/help/en/about.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package GPL Cart core
+ * @author Iurii Makukh <gplcart.software@gmail.com>
+ * @copyright Copyright (c) 2015, Iurii Makukh
+ * @license https://www.gnu.org/licenses/gpl.html GNU/GPLv3
+ */
+?>
+<!-- {
+    "title": "About",
+    "teaser": "",
+    "weight": -99
+} -->
+Version : <?php echo GC_VERSION; ?>
