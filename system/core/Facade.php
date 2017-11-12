d203af0528f1cb38240478552ad6af0347402b84
GplCart <gplcart.software@gmail.com>
2017-11-12 12:18:26 +0200

    Refactoring Facade

--- system/core/Facade.php
+++ system/core/Facade.php
@@ -9,7 +9,8 @@
 
 namespace gplcart\core;
 
-use gplcart\core\helpers\Session as SessionHelper;
+use gplcart\core\helpers\Url as UrlHelper,
+    gplcart\core\helpers\Session as SessionHelper;
 
 /**
  * Provides methods to route incoming requests and setup the system
@@ -24,23 +25,62 @@ class Facade
     protected $config;
 
     /**
+     * Hook instance
+     * @var \gplcart\core\Hook $hook
+     */
+    protected $hook;
+
+    /**
+     * CLI router class instance
+     * @var \gplcart\core\CliRoute $cli
+     */
+    protected $cli;
+
+    /**
+     * HTTP router class instance
+     * @var \gplcart\core\Route
+     */
+    protected $route;
+
+    /**
+     * URL helper class instance
+     * @var \gplcart\core\helpers\Url $url
+     */
+    protected $url;
+
+    /**
+     * Session helper class instance
+     * @var \gplcart\core\helpers\Session $session
+     */
+    protected $session;
+
+    /**
      * @param Config $config
      * @param Hook $hook
+     * @param CliRoute $cli
+     * @param Route $route
      * @param SessionHelper $session
+     * @param UrlHelper $url
      */
-    public function __construct(Config $config, Hook $hook, SessionHelper $session)
+    public function __construct(Config $config, Hook $hook, CliRoute $cli, Route $route,
+            SessionHelper $session, UrlHelper $url)
     {
-        $session->start();
-
+        $this->url = $url;
+        $this->cli = $cli;
+        $this->hook = $hook;
+        $this->route = $route;
         $this->config = $config;
+        $this->session = $session;
+
+        $this->session->init();
         $this->config->init();
 
-        $hook->registerAll();
-        $hook->attach('construct', $this);
+        $this->hook->registerAll();
+        $this->hook->attach('construct', $this);
     }
 
     /**
-     * Routes command line requests
+     * Routes CLI commands
      */
     public function routeCli()
     {
@@ -48,9 +88,7 @@ class Facade
             throw new \Exception('CLI access is disabled!');
         }
 
-        /* @var $route \gplcart\core\CliRoute */
-        $route = Container::get('gplcart\\core\\CliRoute');
-        $route->process();
+        $this->cli->process();
     }
 
     /**
@@ -58,16 +96,10 @@ class Facade
      */
     public function routeHttp()
     {
-        /* @var $route \gplcart\core\Route */
-        $route = Container::get('gplcart\\core\\Route');
-
-        /* @var $url \gplcart\core\helpers\Url */
-        $url = Container::get('gplcart\\core\\helpers\\Url');
-
-        if ($this->config->isInitialized() || $url->isInstall()) {
-            $route->output();
+        if ($this->config->isInitialized() || $this->url->isInstall()) {
+            $this->route->output();
         } else {
-            $url->redirect('install');
+            $this->url->redirect('install');
         }
     }
 
