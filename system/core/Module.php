88280048ac46d6b0c81b69574aba81dbe523dda1
GplCart <gplcart.software@gmail.com>
2017-11-14 13:33:29 +0200

    Add Module::deleteModuleSettings()

--- system/core/Module.php
+++ system/core/Module.php
@@ -157,4 +157,17 @@ abstract class Module
         return "$directory/templates/$template";
     }
 
+    /**
+     * Delete all module settings
+     * @param string $module_id
+     */
+    protected function deleteModuleSettings($module_id)
+    {
+        foreach (array_keys($this->config->select()) as $key) {
+            if (strpos($key, "module_{$module_id}_") === 0) {
+                $this->config->reset($key);
+            }
+        }
+    }
+
 }
