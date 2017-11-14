aa488b2c823131dc3865595fb92016f735388869
GplCart <gplcart.software@gmail.com>
2017-11-14 13:33:55 +0200

    Refactoring Backend module class

--- system/modules/backend/Backend.php
+++ system/modules/backend/Backend.php
@@ -28,26 +28,42 @@ class Backend extends Module
 
     /**
      * Implements hook "theme"
-     * @param \gplcart\core\controllers\backend\Controller $controller
+     * @param \gplcart\core\Controller $controller
      */
     public function hookTheme($controller)
     {
-        if ($controller->isCurrentTheme('backend')) {
-
-            $controller->addAssetLibrary('jquery_ui');
-            $controller->addAssetLibrary('bootstrap');
-            $controller->addAssetLibrary('html5shiv', array('aggregate' => false, 'condition' => 'if lt IE 9'));
-            $controller->addAssetLibrary('respond', array('aggregate' => false, 'condition' => 'if lt IE 9'));
-
-            $controller->setJs($this->getAsset('backend', 'common.js'));
-            $controller->addAssetLibrary('font_awesome');
-            $controller->setCss($this->getAsset('backend', 'style.css'));
-
-            $controller->setMeta(array('charset' => 'utf-8'));
-            $controller->setMeta(array('http-equiv' => 'X-UA-Compatible', 'content' => 'IE=edge'));
-            $controller->setMeta(array('name' => 'viewport', 'content' => 'width=device-width, initial-scale=1'));
-            $controller->setMeta(array('name' => 'author', 'content' => 'GPLCart'));
+        if ($controller->isCurrentTheme('backend') && !$controller->isInternalRoute()) {
+            $this->setThemeAssets($controller);
+            $this->setThemeMetaTags($controller);
         }
+    }
+
+    /**
+     * Sets theme specific assets
+     * @param \gplcart\core\Controller $controller
+     */
+    protected function setThemeAssets($controller)
+    {
+        $controller->addAssetLibrary('jquery_ui');
+        $controller->addAssetLibrary('bootstrap');
+        $controller->addAssetLibrary('html5shiv', array('aggregate' => false, 'condition' => 'if lt IE 9'));
+        $controller->addAssetLibrary('respond', array('aggregate' => false, 'condition' => 'if lt IE 9'));
+
+        $controller->setJs($this->getAsset('backend', 'common.js'));
+        $controller->addAssetLibrary('font_awesome');
+        $controller->setCss($this->getAsset('backend', 'style.css'));
+    }
+
+    /**
+     * Sets meta tags
+     * @param \gplcart\core\Controller $controller
+     */
+    protected function setThemeMetaTags($controller)
+    {
+        $controller->setMeta(array('charset' => 'utf-8'));
+        $controller->setMeta(array('http-equiv' => 'X-UA-Compatible', 'content' => 'IE=edge'));
+        $controller->setMeta(array('name' => 'viewport', 'content' => 'width=device-width, initial-scale=1'));
+        $controller->setMeta(array('name' => 'author', 'content' => 'GPLCart'));
     }
 
 }
