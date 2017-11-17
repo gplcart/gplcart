ee03a371fbe42c4cacce3eca1bd3ec8c4c2536ab
GplCart <gplcart.software@gmail.com>
2017-11-17 13:05:23 +0200

    Update related products block template

--- system/modules/frontend/templates/product/related.php
+++ system/modules/frontend/templates/product/related.php
@@ -9,10 +9,17 @@
  */
 ?>
 <?php if(!empty($products)) { ?>
-<div id="panel-related-products" class="panel panel-default panel-borderless related-products">
-  <div class="panel-heading"><h4 class="panel-title"><?php echo $this->text('Related'); ?></h4></div>
+<div id="panel-related-products" class="panel panel-default panel-borderless related-products<?php echo empty($pager) ? '' : ' has-pager'; ?>">
+  <div class="panel-heading clearfix">
+    <h4 class="panel-title pull-left"><?php echo $this->text('Related'); ?></h4>
+    <?php if(!empty($pager)) { ?>
+    <div class="pull-right">
+      <?php echo $pager; ?>
+    </div>
+    <?php } ?>
+  </div>
   <div class="panel-body">
-    <div class="row products">
+    <div class="row products row-no-padding">
       <?php foreach($products as $product) { ?>
       <?php echo $product['rendered']; ?>
       <?php } ?>
