2dc18c359598625203b3efbfcf6c94bbe83cfc86
GplCart <gplcart.software@gmail.com>
2017-11-25 21:54:01 +0200

    Add oauth template

---
+++ system/modules/frontend/templates/common/oauth.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package GPL Cart core
+ * @author Iurii Makukh <gplcart.software@gmail.com>
+ * @copyright Copyright (c) 2015, Iurii Makukh
+ * @license https://www.gnu.org/licenses/gpl.html GNU/GPLv3
+ * @var $this \gplcart\core\controllers\backend\Controller
+ * To see available variables <?php print_r(get_defined_vars()); ?>
+ */
+?>
+<?php if (!empty($buttons)) { ?>
+<div class="oauth-buttons">
+  <?php foreach ($buttons as $button) { ?>
+  <?php echo $button['rendered']; ?>
+  <?php } ?>
+</div>
+<?php } ?>
