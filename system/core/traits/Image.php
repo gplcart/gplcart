cb895e978afa580dc7a4de4335d3610c9550d579
GplCart <gplcart.software@gmail.com>
2017-11-25 21:52:58 +0200

    Update Image trait

--- system/core/traits/Image.php
+++ system/core/traits/Image.php
@@ -19,65 +19,55 @@ trait Image
 
     /**
      * Adds images to an entity
-     * @param \gplcart\core\Database $db
-     * @param \gplcart\core\models\File $model
      * @param array $data
+     * @param \gplcart\core\models\File $file_model
+     * @param \gplcart\core\models\Translation $translation_model
      * @param string $entity
      * @param null|string $lang
      */
-    protected function attachImagesTrait($db, $model, &$data, $entity, $lang = null)
+    public function attachImages(&$data, $file_model, $translation_model, $entity, $lang = null)
     {
         if (!empty($data)) {
-            $images = $this->getImagesTrait($model, $data, "{$entity}_id");
+
+            $options = array(
+                'order' => 'asc',
+                'sort' => 'weight',
+                'file_type' => 'image',
+                'id_key' => "{$entity}_id",
+                'id_value' => $data["{$entity}_id"]
+            );
+
+            $images = (array) $file_model->getList($options);
+
             foreach ($images as &$image) {
-                $this->attachTranslationTrait($db, $image, 'file', $lang);
+                $this->attachTranslations($image, $translation_model, 'file', $lang);
             }
+
             $data['images'] = $images;
         }
     }
 
     /**
-     * Returns an array of images for the given entity
-     * @param \gplcart\core\models\File $model
-     * @param array $data
-     * @param string $key
-     * @return array
-     */
-    protected function getImagesTrait($model, array $data, $key)
-    {
-        $options = array(
-            'order' => 'asc',
-            'sort' => 'weight',
-            'file_type' => 'image',
-            'id_key' => $key,
-            'id_value' => $data[$key]
-        );
-
-        return (array) $model->getList($options);
-    }
-
-    /**
      * Set entity images
-     * @param \gplcart\core\models\File $model
      * @param array $data
+     * @param \gplcart\core\models\File $file_model
      * @param string $entity
      * @return array
      */
-    protected function setImagesTrait($model, array &$data, $entity)
+    public function setImages(array &$data, $file_model, $entity)
     {
-        if (empty($data['images'])) {
+        if (empty($data['images']) || empty($data["{$entity}_id"])) {
             return array();
         }
 
         foreach ($data['images'] as &$image) {
-
-            if (!empty($image['file_id'])) {
-                $model->update($image['file_id'], $image);
-                continue;
+            if (empty($image['file_id'])) {
+                $image['id_key'] = "{$entity}_id";
+                $image['id_value'] = $data["{$entity}_id"];
+                $image['file_id'] = (int) $file_model->add($image);
+            } else {
+                $file_model->update($image['file_id'], $image);
             }
-
-            $file = $image + array('id_key' => "{$entity}_id", 'id_value' => $data["{$entity}_id"]);
-            $image['file_id'] = (int) $model->add($file);
         }
 
         return $data['images'];
