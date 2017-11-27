1a66e40181af76686fc572cac2a5e5294eef82ed
GplCart <gplcart.software@gmail.com>
2017-11-27 03:17:42 +0200

    Update test bootstrap.php

--- tests/phpunit/support/bootstrap.php
+++ tests/phpunit/support/bootstrap.php
@@ -6,13 +6,14 @@
  * @copyright Copyright (c) 2015, Iurii Makukh
  * @license https://www.gnu.org/licenses/gpl.html GNU/GPLv3
  */
+ini_set('error_reporting', E_ALL);
+ini_set('memory_limit', '-1');
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+
 require_once __DIR__ . '/../../../system/bootstrap.php';
 
 if (!class_exists('PHPUnit_Extensions_Database_TestCase')) {
     throw new \Exception('Looks like DBUnit extension not installed. See https://github.com/sebastianbergmann/dbunit');
 }
 
-ini_set('error_reporting', E_ALL);
-ini_set('memory_limit', '-1');
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
