db71236553a95bb945f3331cccd8e8683d177559
GplCart <gplcart.software@gmail.com>
2017-12-27 11:34:08 +0200

    Fix arguments in ini_set()

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
  * @license https://www.gnu.org/licenses/gpl.html GNU/GPLv3
  */
 error_reporting(E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
 
 require 'system/bootstrap.php';
 
