8748d921c6d298cffed86a399a2f9e6b9e32e58a
GplCart <gplcart.software@gmail.com>
2018-01-07 18:40:29 +0200

    Add Cron model

--- system/core/controllers/frontend/Cron.php
+++ system/core/controllers/frontend/Cron.php
@@ -9,9 +9,7 @@
 
 namespace gplcart\core\controllers\frontend;
 
-use gplcart\core\Logger;
-use gplcart\core\models\Report as ReportModel,
-    gplcart\core\models\History as HistoryModel;
+use gplcart\core\models\Cron as CronModel;
 use gplcart\core\controllers\frontend\Controller as FrontendController;
 
 /**
@@ -21,35 +19,19 @@ class Cron extends FrontendController
 {
 
     /**
-     * Logger class instance
-     * @var \gplcart\core\Logger $logger
+     * Cron model instance
+     * @var \gplcart\core\models\Cron $cron
      */
-    protected $logger;
+    protected $cron;
 
     /**
-     * Report model instance
-     * @var \gplcart\core\models\Report $report
+     * @param CronModel $cron
      */
-    protected $report;
-
-    /**
-     * History model instance
-     * @var \gplcart\core\models\History $history
-     */
-    protected $history;
-
-    /**
-     * @param Logger $logger
-     * @param ReportModel $report
-     * @param HistoryModel $history
-     */
-    public function __construct(Logger $logger, ReportModel $report, HistoryModel $history)
+    public function __construct(CronModel $cron)
     {
         parent::__construct();
 
-        $this->logger = $logger;
-        $this->report = $report;
-        $this->history = $history;
+        $this->cron = $cron;
     }
 
     /**
@@ -58,15 +40,7 @@ class Cron extends FrontendController
     public function executeCron()
     {
         $this->controlAccessExecuteCron();
-
-        register_shutdown_function(array($this, 'shutdownHandlerCron'));
-        ini_set('max_execution_time', 0);
-        $this->logger->log('cron', 'Cron has started', 'info', true);
-
-        $this->processTasksCron();
-
-        $this->hook->attach('cron', $this);
-        $this->config->set('cron_last_run', GC_TIME);
+        $this->cron->run();
         $this->response->outputHtml($this->text('Cron has started'));
     }
 
@@ -75,75 +49,9 @@ class Cron extends FrontendController
      */
     protected function controlAccessExecuteCron()
     {
-        if (strcmp($this->getQuery('key', ''), $this->config('cron_key', '')) !== 0) {
+        if (strcmp($this->getQuery('key', ''), $this->cron->getKey()) !== 0) {
             $this->response->outputError403(false);
         }
-    }
-
-    /**
-     * Processes all defined tasks
-     */
-    protected function processTasksCron()
-    {
-        $this->deleteExpiredHistoryCron();
-        $this->deleteExpiredFilesCron();
-        $this->deleteExpiredLogsCron();
-        $this->checkFilesystemCron();
-    }
-
-    /**
-     * Deletes expired records from history table
-     */
-    protected function deleteExpiredHistoryCron()
-    {
-        $this->history->deleteExpired();
-    }
-
-    /**
-     * Deletes expired log records
-     */
-    protected function deleteExpiredLogsCron()
-    {
-        $this->report->deleteExpired();
-    }
-
-    /**
-     * Deletes old expired files
-     */
-    protected function deleteExpiredFilesCron()
-    {
-        foreach (gplcart_file_scan_recursive(GC_DIR_PRIVATE_TEMP) as $file) {
-            if (strpos(basename($file), '.') !== 0) { // Ignore hidden files
-                gplcart_file_delete_recursive($file);
-            }
-        }
-    }
-
-    /**
-     * Checks file system and logs errors
-     * @return boolean
-     */
-    protected function checkFilesystemCron()
-    {
-        $result = $this->report->checkFilesystem();
-
-        if ($result === true) {
-            return true;
-        }
-
-        foreach ((array) $result as $message) {
-            $this->logger->log('system_status', array('message' => $message), 'warning');
-        }
-
-        return false;
-    }
-
-    /**
-     * Handles PHP shutdown
-     */
-    public function shutdownHandlerCron()
-    {
-        $this->config->set('cron_last_run', GC_TIME);
     }
 
 }
---
+++ system/core/models/Cron.php
@@ -0,0 +1,148 @@
+<?php
+
+/**
+ * @package GPL Cart core
+ * @author Iurii Makukh <gplcart.software@gmail.com>
+ * @copyright Copyright (c) 2015, Iurii Makukh
+ * @license https://www.gnu.org/licenses/gpl.html GNU/GPLv3
+ */
+
+namespace gplcart\core\models;
+
+use Exception;
+use gplcart\core\Hook,
+    gplcart\core\Logger,
+    gplcart\core\Config;
+use gplcart\core\models\Report as ReportModel,
+    gplcart\core\models\History as HistoryModel;
+
+/**
+ * Manages basic behaviors and data related to scheduled tasks
+ */
+class Cron
+{
+
+    /**
+     * Hook class instance
+     * @var \gplcart\core\Hook $hook
+     */
+    protected $hook;
+
+    /**
+     * Logger class instance
+     * @var \gplcart\core\Logger $logger
+     */
+    protected $logger;
+
+    /**
+     * Config class instance
+     * @var \gplcart\core\Config $config
+     */
+    protected $config;
+
+    /**
+     * Report model instance
+     * @var \gplcart\core\models\Report $report
+     */
+    protected $report;
+
+    /**
+     * History model instance
+     * @var \gplcart\core\models\History $history
+     */
+    protected $history;
+
+    /**
+     * @param Logger $logger
+     * @param Hook $hook
+     * @param Config $config
+     * @param ReportModel $report
+     * @param HistoryModel $history
+     */
+    public function __construct(Logger $logger, Hook $hook, Config $config, ReportModel $report,
+            HistoryModel $history)
+    {
+        $this->hook = $hook;
+        $this->config = $config;
+        $this->logger = $logger;
+
+        $this->report = $report;
+        $this->history = $history;
+    }
+
+    /**
+     * Run cron tasks
+     * @return bool
+     */
+    public function run()
+    {
+        $result = null;
+        $this->hook->attach('cron.run.before', $result, $this);
+
+        if (isset($result)) {
+            return (bool) $result;
+        }
+
+        ini_set('max_execution_time', 0);
+        register_shutdown_function(array($this, 'shutdownHandler'));
+
+        $this->logger->log('cron', 'Cron has started', 'info');
+
+        try {
+            $result = $this->process();
+        } catch (Exception $ex) {
+            $this->logger->log('cron', $ex->getMessage(), 'danger', false);
+            $result = false;
+        }
+
+        $this->hook->attach('cron.run.after', $result, $this);
+        $this->config->set('cron_last_run', GC_TIME);
+
+        return (bool) $result;
+    }
+
+    /**
+     * Processes all defined tasks
+     * @return bool
+     */
+    public function process()
+    {
+        $this->report->deleteExpired();
+        $this->history->deleteExpired();
+
+        // Delete files in temporary directory
+        foreach (gplcart_file_scan_recursive(GC_DIR_PRIVATE_TEMP) as $file) {
+            if (strpos(basename($file), '.') !== 0) { // Ignore hidden files
+                gplcart_file_delete_recursive($file);
+            }
+        }
+
+        $result = $this->report->checkFilesystem();
+
+        if ($result !== true) {
+            foreach ((array) $result as $message) {
+                $this->logger->log('system_status', array('message' => $message), 'warning');
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Returns the cron key
+     * @return string
+     */
+    public function getKey()
+    {
+        return $this->config->get('cron_key', '');
+    }
+
+    /**
+     * Handles PHP shutdown
+     */
+    public function shutdownHandler()
+    {
+        $this->config->set('cron_last_run', GC_TIME);
+    }
+
+}
