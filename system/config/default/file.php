c72865f76c385434bd9be0964cb30865486e2b6b
GplCart <gplcart.software@gmail.com>
2018-03-01 13:02:05 +0200

    Add upload path in file config

--- system/config/default/file.php
+++ system/config/default/file.php
@@ -9,19 +9,23 @@
 return array(
     'image' => array(
         'extensions' => array('jpg', 'jpeg', 'gif', 'png'),
-        'validator' => 'image'
+        'validator' => 'image',
+        'path' => GC_DIR_IMAGE
     ),
     'json' => array(
         'extensions' => array('json'),
-        'validator' => 'json'
+        'validator' => 'json',
+        'path' => GC_DIR_UPLOAD
     ),
     'csv' => array(
         'extensions' => array('csv'),
-        'validator' => 'csv'
+        'validator' => 'csv',
+        'path' => GC_DIR_UPLOAD
     ),
     'zip' => array(
         'extensions' => array('zip'),
-        'validator' => 'zip'
+        'validator' => 'zip',
+        'path' => GC_DIR_UPLOAD
     )
 );
 
